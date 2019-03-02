8c344156dfaa1b340b1c179a11933612f4e055c8
DmK78 <Ek193burg>
2019-03-02 13:12:36 +0500

    Change email to different apps

--- app/src/main/java/com/example/musicshop/OrderActivity.java
+++ app/src/main/java/com/example/musicshop/OrderActivity.java
@@ -1,12 +1,17 @@
 package com.example.musicshop;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    String [] addresses={"rushoreca@inbox.ru"};
+    String subject= "Order from music shop";
+    String emailText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +26,25 @@ public class OrderActivity extends AppCompatActivity {
         double orderPrice = receivedOrderIntent.getDoubleExtra("orderPrice", 0);
         double price = receivedOrderIntent.getDoubleExtra("price", 0);
 
+        emailText="Customer name: " + userName + "\n" + "Goods name: " + goodsName + "\n" + "Quantity: "
+                + quantity + "\n" + "Price: " + price + "\n" + "Order price: " + orderPrice;
 
         TextView orderTextView = findViewById(R.id.orderTextView);
-        orderTextView.setText("Customer name: " + userName + "\n" + "Goods name: " + goodsName + "\n" + "Quantity: " + quantity + "\n" + "Price: " + price + "\n" + "Order price: " + orderPrice);
+        orderTextView.setText(emailText);
     }
 
     public void submitOrder(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
+
+
 }
